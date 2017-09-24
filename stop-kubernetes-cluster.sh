59341c2db79e5393df4f41b0e446bab6550bf34e
Jeroen Visser <jjerroennvvisserr@gmail.com>
2017-09-24 16:51:35 +0200

    Fix for index-server env variable and stop-cluster script.

---
+++ stop-kubernetes-cluster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl delete --namespace=money-tracker -f namespace.yaml
+kubectl delete --namespace=money-tracker -f secret.yaml
+kubectl delete --namespace=money-tracker -f money-tracker-database-deployment.yaml
+kubectl delete --namespace=money-tracker -f database-service.yaml
+kubectl delete --namespace=money-tracker -f money-tracker-index-server-deployment.yaml
+kubectl delete --namespace=money-tracker -f index-server-service.yaml
+
